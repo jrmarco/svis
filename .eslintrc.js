11603770599d47acfab4f9dfe94c1fc5f334545d
Marco <27374422+jrmarco@users.noreply.github.com>
2022-05-29 20:27:18 +0200

    chore: linting config file

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
